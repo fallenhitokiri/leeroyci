e9d7fd0d2f1541d8024089d5f5d98be8e3f6962c
Timo Zimmermann <timo@screamingatmyscreen.com>
2014-05-01 16:31:43 +0200

    add methods to Job to check the build status

--- logging/job.go
+++ logging/job.go
@@ -15,3 +15,23 @@ type Job struct {
 	Name       string
 	Email      string
 }
+
+// Returns either the exit code of the triggered command or 0 if the command
+// finished successfully.
+func (j *Job) Status() string {
+	code := "0"
+
+	if j.ReturnCode != nil {
+		code = j.ReturnCode.Error()
+	}
+
+	return code
+}
+
+// Returns true if the build was successful.
+func (j *Job) Success() bool {
+	if j.Status() == "0" {
+		return true
+	}
+	return false
+}
--- logging/job_test.go
+++ logging/job_test.go
@@ -1,1 +1,34 @@
 package logging
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestStatus(t *testing.T) {
+	j := Job{}
+
+	if j.Status() != "0" {
+		t.Error("Wrong status", j.Status())
+	}
+
+	j.ReturnCode = errors.New("foo")
+
+	if j.Status() == "0" {
+		t.Error("Wrong status", j.Status())
+	}
+}
+
+func TestSuccess(t *testing.T) {
+	j := Job{}
+
+	if j.Success() == false {
+		t.Error("Returned error for successful build")
+	}
+
+	j.ReturnCode = errors.New("foo")
+
+	if j.Success() == true {
+		t.Error("Returned no error for failed build")
+	}
+}
