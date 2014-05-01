0f8ed76bd1a4330bfa9791805e671055a9690f12
Timo Zimmermann <timo@screamingatmyscreen.com>
2014-05-01 16:36:31 +0200

    remove old notification function

--- build/build.go
+++ build/build.go
@@ -7,7 +7,6 @@ import (
 	"ironman/logging"
 	"ironman/notification"
 	"log"
-	"net/smtp"
 	"os/exec"
 )
 
@@ -44,26 +43,4 @@ func call(app string, repo string, branch string) (string, error) {
 	cmd := exec.Command(app, repo, branch)
 	out, err := cmd.CombinedOutput()
 	return string(out), err
-}
-
-// Notify a pusher that a build failed.
-func notify_fail(cmd config.Command, repo string, branch string, out string,
-	err error, c *config.Config, name string, mail string) error {
-	mime := "MIME-version: 1.0;Content-Type: text/html; charset=\"UTF-8\";\n\n"
-	subject := "Build Failed!\n"
-	body := repo + "\n" + branch + "\n" + cmd.Name + "\n\n"
-	body = body + err.Error() + "\n\n" + out
-	// TODO: this should look better
-
-	msg := []byte(subject + mime + body)
-
-	auth := smtp.PlainAuth("", c.EmailUser, c.EmailPassword, c.EmailHost)
-
-	err = smtp.SendMail(c.MailServer(), auth, c.EmailFrom, []string{mail}, msg)
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	return err
 }
