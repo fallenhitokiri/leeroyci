79b01dc2f99c1968aa6b4230f9122888ee8733ce
Timo Zimmermann <timo@screamingatmyscreen.com>
2014-05-01 18:07:44 +0200

    run all notifications when a build finished

--- notification/notification.go
+++ notification/notification.go
@@ -1,9 +1,10 @@
+// Notifications to send after a build process finished.
 package notification
 
 import (
 	"ironman/config"
 	"ironman/logging"
-	//"log"
+	"log"
 )
 
 // Run all notifications configured for a
@@ -11,10 +12,26 @@ func Notify(c *config.Config, j *logging.Job) {
 	// always notify the person who comitted
 	go email(c, j, j.Email)
 
-	/*config, err := c.ConfigForRepo(job.URL)
+	repo, err := c.ConfigForRepo(j.URL)
 
 	if err != nil {
-		log.Println("could not find repo", job.URL)
+		log.Println("could not find repo", j.URL)
 		return
-	}*/
+	}
+
+	for _, n := range repo.Notify {
+		if n.Service == "email" {
+			// Arguments for email are the mail addresses to notify
+			for _, mail := range n.Arguments {
+				go email(c, j, mail)
+			}
+			continue
+		}
+
+		if n.Service == "slack" {
+			// No arguments for Slack
+			go slack(c, j)
+			continue
+		}
+	}
 }
