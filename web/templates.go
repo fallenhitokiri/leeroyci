b1a85c1be6e989a64901ada38c8c85425a122a77
Timo Zimmermann <timo@screamingatmyscreen.com>
2014-05-30 22:54:47 +0200

    basic template

--- web/templates.go
+++ web/templates.go
@@ -10,21 +10,34 @@ var templateStatus = `
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     </head>
     <body>
-        {{range .Jobs}}
-            <h1>{{.URL}} - {{.Branch}}</h1>
-            <p>Commit: {{.Commit}}</p>
-            <p>Timestamp: {{.Timestamp}}</p>
-            <p>By: {{.Name}} <{{.Email}}></p>
-            
-            <h4>Tasks</h4>
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Leeroy</a>
+                </div>
+            </div>
+        </div>
 
-            {{range .Tasks}}
-                <h6>Command: {{.Command}}</h6>
-                <p>Return code: {{.Return}}</p>
-                <p>Output: {{.Output}}</p>
-                <hr />
+        <div class="container" style="padding:80px 0 0 0;">
+            {{range .Jobs}}
+            <div class="panel {{if .Success}}panel-success{{else}}panel-danger{{end}}">
+                <div class="panel-heading">
+                    <h3 class="panel-title">{{.URL}} - {{.Branch}}</h3>
+                </div>
+                <div class="panel-body">
+                    {{range .Tasks}}
+                    <h6>{{.Command}} {{if .Return}}- {{.Return}}{{end}}</h6>
+                    <p><code>{{.Output}}</code></p>
+                    {{end}}
+                </div>
+                <div class="panel-footer">
+                    <a href="{{.CommitURL}}"><span class="label label-primary">{{.Commit}}</span></a>
+                    <span class="label label-default">{{.Timestamp}}</span>
+                    <span class="label label-default">{{.Name}} <{{.Email}}></span>
+                </div>
+            </div>
             {{end}}
-        {{end}}
+        </div>
     </body>
 </html>
 `
