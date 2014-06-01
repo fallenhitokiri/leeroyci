513856aff597f44bc2f1eddad1b83e193978a8c0
Timo Zimmermann <timo@screamingatmyscreen.com>
2014-06-01 17:07:48 +0200

    adsf

--- leeroy.go
+++ leeroy.go
@@ -13,6 +13,13 @@ import (
 
 var cfgFlag = flag.String("config", "leeroy.json", "JSON formatted config")
 
+func makeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r)
+		fn(w, r)
+	}
+}
+
 func main() {
 	flag.Parse()
 
@@ -25,12 +32,12 @@ func main() {
 
 	log.Println("leeroy up an running!")
 
-	http.HandleFunc("/callback/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/callback/", makeHandler(func(w http.ResponseWriter, r *http.Request) {
 		callbacks.Callback(w, r, jobs, &c, b)
-	})
-	http.HandleFunc("/status/", func(w http.ResponseWriter, r *http.Request) {
+	}))
+	http.HandleFunc("/status/", makeHandler(func(w http.ResponseWriter, r *http.Request) {
 		web.Status(w, r, &c, b)
-	})
+	}))
 	http.HandleFunc("/status/repo/", func(w http.ResponseWriter, r *http.Request) {
 		web.Repo(w, r, &c, b)
 	})
