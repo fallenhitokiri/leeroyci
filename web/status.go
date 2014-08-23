08aaa7f9ec6fb41bbca56b76f9f824bd7f0ecb36
Timo Zimmermann <timo@screamingatmyscreen.com>
2014-08-23 09:11:20 +0200

    refactor helpers

--- web/status.go
+++ web/status.go
@@ -2,14 +2,9 @@
 package web
 
 import (
-	"encoding/hex"
-	"encoding/json"
-	"html/template"
 	"leeroy/config"
 	"leeroy/logging"
-	"log"
 	"net/http"
-	"strings"
 )
 
 // View that shows all builds ever done.
@@ -74,53 +69,4 @@ func Badge(rw http.ResponseWriter, req *http.Request, c *config.Config,
 	rw.Header().Set("Content-Type", "image/svg+xml")
 	rw.Write(svg)
 	req.Body.Close()
-}
-
-// Get a template and execute it.
-func render(rw http.ResponseWriter, req *http.Request, jobs []logging.Job) {
-	f := responseFormat(req)
-
-	if f == "json" {
-		res, err := json.Marshal(jobs)
-
-		if err != nil {
-			log.Println("error marshal", err)
-			rw.Header().Set("Content-Type", "application/json")
-			rw.Write([]byte(`{"error": "marshal not possible"}`))
-			req.Body.Close()
-		} else {
-			rw.Header().Set("Content-Type", "application/json")
-			rw.Write(res)
-			req.Body.Close()
-		}
-		return
-	}
-
-	t := template.New("status")
-	t, _ = t.Parse(standard)
-	t.Execute(
-		rw,
-		map[string]interface{}{
-			"Jobs": jobs,
-		},
-	)
-}
-
-// Splits a request path and returns the first part after the endpoint.
-// This is usually the hex string of the repository.
-func splitFirst(path string) string {
-	s := strings.Split(path, "/")[3]
-	r, err := hex.DecodeString(s)
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	return string(r)
-}
-
-// Splits a request path and returns the first part after the endpoint.
-// This is likely the branch name or commit sha1.
-func splitSecond(path string) string {
-	return strings.Split(path, "/")[4]
 }
