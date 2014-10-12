2882257bb27eeae538c2e1a1fb5fcbd354536074
Timo Zimmermann <timo@screamingatmyscreen.com>
2014-10-12 14:49:54 +0200

    add continuous deployment trigger

---
+++ deployment/continuous.go
@@ -0,0 +1,30 @@
+// Handle finished build jobs and trigger a deployment if configured.
+package deployment
+
+import (
+	"leeroy/config"
+	"leeroy/logging"
+	"log"
+)
+
+// Check if the branch that was just build is configured for continuous
+// deployment and trigger a deploy.
+// Returns true if a deployment is triggered.
+func ContinuousDeploy(j *logging.Job, c *config.Config) bool {
+	r, err := c.ConfigForRepo(j.URL)
+
+	if err != nil {
+		log.Println("Cannot deploy ", j.Branch, " repository not found.")
+		return false
+	}
+
+	for _, d := range r.Deploy {
+		if d.Branch == j.Branch {
+			log.Println("deploying ", d.Branch)
+			go Deploy()
+			return true
+		}
+	}
+
+	return false
+}
---
+++ deployment/deploy.go
@@ -0,0 +1,5 @@
+package deployment
+
+func Deploy() {
+
+}
