24e9d2b0e9380229580cd4ae1ee7b96be2b98c43
Timo Zimmermann <timo@screamingatmyscreen.com>
2014-10-14 23:08:59 +0200

    add test for DeployTarget

--- config/config_test.go
+++ config/config_test.go
@@ -78,3 +78,35 @@ func TestIdentifier(t *testing.T) {
 		t.Error("Wrong identifier", r2.Identifier())
 	}
 }
+
+func TestDeployTarget(t *testing.T) {
+	r := Repository{}
+	d1 := Deploy{
+		Branch: "foo",
+	}
+	d2 := Deploy{
+		Branch: "bar",
+	}
+
+	r.Deploy = append(r.Deploy, d1)
+
+	_, err := r.DeployTarget("foo")
+
+	if err != nil {
+		t.Error("Got an error, should not:", err)
+	}
+
+	_, err = r.DeployTarget("bar")
+
+	if err == nil {
+		t.Error("Got no error, but should!")
+	}
+
+	r.Deploy = append(r.Deploy, d2)
+
+	_, err = r.DeployTarget("bar")
+
+	if err != nil {
+		t.Error("Got an error, should not:", err)
+	}
+}
