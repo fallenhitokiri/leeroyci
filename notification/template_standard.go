6c19ae6a943754f374f30eff1a76e63f0230f42f
Timo Zimmermann <timo@screamingatmyscreen.com>
2015-01-11 18:14:36 +0100

    add deploy notifications

--- notification/template_standard.go
+++ notification/template_standard.go
@@ -5,8 +5,16 @@ package notification
 // Map of all available templates.
 // Key = identifier used by LeeroyCI, value = template string.
 var templates = map[string]string{
-	KindBuild: templateBuild,
+	KindBuild:       templateBuild,
+	KindDeployStart: templateDeployStart,
+	KindDeployDone:  templateDeployDone,
 }
 
 // Template for build notifications.
 var templateBuild = "Repository: {{.Repo}} Branch: {{.Branch}} by {{.Name}} <{{.Email}}> -> Build {{if .Status}}success{{else}}failed{{end}}\nDetails: {{.URL}}"
+
+// Template before a deployment task starts.
+var templateDeployStart = "Deploying {{.Branch}} for {{.Repo}}"
+
+// Template after a deployment task finished.
+var templateDeployDone = "Deployment {{if .Deploy}}successful{{else}}failed{{end}} {{.Branch}} for {{.Repo}}\nDetails: {{.URL}}"
