86b4c29d7336d58e39eb248470a29444c4be8045
Timo Zimmermann <timo@screamingatmyscreen.com>
2015-06-21 14:37:07 +0200

    refactor login view

--- web/login.go
+++ web/login.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gorilla/schema"
@@ -13,54 +14,54 @@ type loginForm struct {
 	Password string `schema:"password"`
 }
 
+func (l loginForm) authenticate(request *http.Request) (string, error) {
+	err := request.ParseForm()
+
+	if err != nil {
+		return "", err
+	}
+
+	decoder := schema.NewDecoder()
+	form := new(loginForm)
+
+	err = decoder.Decode(form, request.PostForm)
+
+	if err != nil {
+		return "", err
+	}
+
+	user, err := database.GetUser(form.Email)
+
+	if err != nil {
+		return "", err
+	}
+
+	auth := database.ComparePassword(form.Password, user.Password)
+
+	if auth == false {
+		return "", errors.New("Username and password do not match.")
+	}
+
+	return user.NewSession(), nil
+}
+
 func viewLogin(w http.ResponseWriter, r *http.Request) {
 	template := "login.html"
 	ctx := make(responseContext)
 
 	if r.Method == "POST" {
-		err := r.ParseForm()
+		sessionID, err := loginForm{}.authenticate(r)
 
-		if err != nil {
-			ctx["error"] = err.Error()
-			render(w, r, template, ctx)
+		if err == nil {
+			session, _ := sessionStore.Get(r, "leeroyci")
+			session.Values["session_key"] = sessionID
+			session.Save(r, w)
+
+			http.Redirect(w, r, "/builds", 302)
 			return
 		}
 
-		decoder := schema.NewDecoder()
-		form := new(loginForm)
-
-		err = decoder.Decode(form, r.PostForm)
-
-		if err != nil {
-			ctx["error"] = err.Error()
-			render(w, r, template, ctx)
-			return
-		}
-
-		user, err := database.GetUser(form.Email)
-
-		if err != nil {
-			ctx["error"] = err.Error()
-			render(w, r, template, ctx)
-			return
-		}
-
-		auth := database.ComparePassword(form.Password, user.Password)
-
-		if auth == false {
-			ctx["error"] = "Authentication failed."
-			render(w, r, template, ctx)
-			return
-		}
-
-		sessionID := user.NewSession()
-
-		session, _ := sessionStore.Get(r, "leeroyci")
-		session.Values["session_key"] = sessionID
-		session.Save(r, w)
-
-		http.Redirect(w, r, "/builds", 302)
-		return
+		ctx["error"] = err.Error()
 	}
 
 	render(w, r, template, ctx)
