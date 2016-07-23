4f46fdb3f6918c9fe53e7b732d0f6ca9f4d16d53
Timo Zimmermann <timo@screamingatmyscreen.com>
2016-07-23 12:52:03 +0200

    project start

--- config/project.go
+++ config/project.go
@@ -1,6 +1,8 @@
 // Package config contains all data models used for LeeroyCI.
 package config
 
+import "errors"
+
 // import "errors"
 
 // Project represents one project / repository used to run jobs for.
@@ -22,6 +24,47 @@ type Project struct {
 	Notifications []*Notification
 }
 
-// func NewProject(name string) (*Project, error)  {
-//
-// }
+// NewProject creates a new project. An AccessKey is also created.
+func NewProject(name, url string, closePR, statusPR bool) (*Project, error) {
+	_, err := ProjectByName(name)
+	if err == nil {
+		return nil, errors.New("Project with this name already exists.")
+	}
+
+	accessKey := generateAccessKey(accessKeyDictionary, accessKeyLength)
+
+	project := &Project{
+		Name:      name,
+		URL:       url,
+		AccessKey: accessKey,
+		ClosePR:   closePR,
+		StatusPR:  statusPR,
+	}
+
+	cfg.Projects = append(cfg.Projects, project)
+
+	return project, nil
+}
+
+// ProjectByName returns a project with the matching name or an error.
+func ProjectByName(name string) (*Project, error) {
+	for _, project := range cfg.Projects {
+		if project.Name == name {
+			return project, nil
+		}
+	}
+
+	return nil, errors.New("Project not found.")
+}
+
+// ProjectByAccessKey returns a project with the matching access key or an
+// error.
+func ProjectByAccessKey(key string) (*Project, error) {
+	for _, project := range cfg.Projects {
+		if project.AccessKey == key {
+			return project, nil
+		}
+	}
+
+	return nil, errors.New("Project not found.")
+}
