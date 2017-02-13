4ffb48915a1d8ec8756fcfe5daecd2d41442fc43
John Nguyen <jnguyen@thefrontiergroup.com.au>
2017-02-13 19:25:06 +0800

    removes alice

--- web/router.go
+++ web/router.go
@@ -6,55 +6,62 @@ import (
 	"github.com/GeertJohan/go.rice"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
-	"github.com/justinas/alice"
 
 	"github.com/fallenhitokiri/leeroyci/websocket"
 )
 
 var sessionStore = sessions.NewCookieStore([]byte("something-very-secret"))
 
+func chainUnauth(next http.HandlerFunc) http.Handler {
+	return middlewareLogging(middlewarePanic(middlewareNoConfig(next)))
+}
+
+func chainAuth(next http.HandlerFunc) http.Handler {
+	return middlewareLogging(middlewarePanic(middlewareNoConfig(middlewareAuth(next))))
+}
+func chainAdmin(next http.HandlerFunc) http.Handler {
+	return middlewareLogging(middlewarePanic(middlewareNoConfig(middlewareAuth(middlewareAdmin(next)))))
+}
+
 // Routes returns a new Goriall router.
 func Routes() *mux.Router {
-	chainUnauth := alice.New(middlewareLogging, middlewarePanic, middlewareNoConfig)
-	chainAuth := alice.New(middlewareLogging, middlewarePanic, middlewareNoConfig, middlewareAuth)
-	chainAdmin := alice.New(middlewareLogging, middlewarePanic, middlewareNoConfig, middlewareAuth, middlewareAdmin)
 
 	router := mux.NewRouter()
-	router.Handle("/setup", chainUnauth.ThenFunc(viewSetup))
-	router.Handle("/login", chainUnauth.ThenFunc(viewLogin))
-	router.Handle("/logout", chainUnauth.ThenFunc(viewLogout))
-	router.Handle("/callback/{service:[a-zA-Z]+}/{secret:[a-zA-Z0-9-]+}", chainUnauth.ThenFunc(viewCallback))
+	router.Handle("/setup", chainUnauth(viewSetup))
+	router.Handle("/login", chainUnauth(viewLogin))
+	router.Handle("/logout", chainUnauth(viewLogout))
+	router.Handle("/callback/{service:[a-zA-Z]+}/{secret:[a-zA-Z0-9-]+}", chainUnauth(viewCallback))
 
-	router.Handle("/", chainAuth.ThenFunc(viewListJobs)).Name("listJobs")
-	router.Handle("/{jid:[0-9]+}", chainAuth.ThenFunc(viewDetailJob))
-	router.Handle("/{jid:[0-9]+}/cancel", chainAuth.ThenFunc(viewCancelJob))
-	router.Handle("/{jid:[0-9]+}/rerun", chainAuth.ThenFunc(viewRerunJob))
-	router.Handle("/search", chainAuth.ThenFunc(viewSearchJobs))
+	router.Handle("/", chainAuth(viewListJobs))
+	router.Handle("/{jid:[0-9]+}", chainAuth(viewDetailJob))
+	router.Handle("/{jid:[0-9]+}/cancel", chainAuth(viewCancelJob))
+	router.Handle("/{jid:[0-9]+}/rerun", chainAuth(viewRerunJob))
+	router.Handle("/search", chainAuth(viewSearchJobs))
 
-	router.Handle("/user/settings", chainAuth.ThenFunc(viewUpdateUser))
-	router.Handle("/user/regenerate-accesskey", chainAuth.ThenFunc(viewRegenrateAccessKey))
+	router.Handle("/user/settings", chainAuth(viewUpdateUser))
+	router.Handle("/user/regenerate-accesskey", chainAuth(viewRegenrateAccessKey))
 
-	router.Handle("/admin/users", chainAdmin.ThenFunc(viewAdminListUsers))
-	router.Handle("/admin/user/create", chainAdmin.ThenFunc(viewAdminCreateUser))
-	router.Handle("/admin/user/{uid:[0-9]+}", chainAdmin.ThenFunc(viewAdminUpdateUser))
-	router.Handle("/admin/user/delete/{uid:[0-9]+}", chainAdmin.ThenFunc(viewAdminDeleteUser))
+	router.Handle("/admin/users", chainAdmin(viewAdminListUsers))
+	router.Handle("/admin/user/create", chainAdmin(viewAdminCreateUser))
+	router.Handle("/admin/user/{uid:[0-9]+}", chainAdmin(viewAdminUpdateUser))
+	router.Handle("/admin/user/delete/{uid:[0-9]+}", chainAdmin(viewAdminDeleteUser))
 
-	router.Handle("/admin/repositories", chainAdmin.ThenFunc(viewAdminListRepositories))
-	router.Handle("/admin/repository/create", chainAdmin.ThenFunc(viewAdminCreateRepository))
-	router.Handle("/admin/repository/{rid:[0-9]+}", chainAdmin.ThenFunc(viewAdminUpdateRepository))
-	router.Handle("/admin/repository/delete/{rid:[0-9]+}", chainAdmin.ThenFunc(viewAdminDeleteRepository))
+	router.Handle("/admin/repositories", chainAdmin(viewAdminListRepositories))
+	router.Handle("/admin/repository/create", chainAdmin(viewAdminCreateRepository))
+	router.Handle("/admin/repository/{rid:[0-9]+}", chainAdmin(viewAdminUpdateRepository))
+	router.Handle("/admin/repository/delete/{rid:[0-9]+}", chainAdmin(viewAdminDeleteRepository))
 
-	router.Handle("/admin/mailserver/update", chainAdmin.ThenFunc(viewAdminUpdateMailserver))
+	router.Handle("/admin/mailserver/update", chainAdmin(viewAdminUpdateMailserver))
 
-	router.Handle("/admin/config/update", chainAdmin.ThenFunc(viewAdminUpdateConfig))
+	router.Handle("/admin/config/update", chainAdmin(viewAdminUpdateConfig))
 
-	router.Handle("/admin/repository/{rid:[0-9]+}/notification/create", chainAdmin.ThenFunc(viewAdminCreateNotification))
-	router.Handle("/admin/repository/{rid:[0-9]+}/notification/{nid:[0-9]+}", chainAdmin.ThenFunc(viewAdminUpdateNotification))
-	router.Handle("/admin/repository/{rid:[0-9]+}/notification/delete/{nid:[0-9]+}", chainAdmin.ThenFunc(viewAdminDeleteNotification))
+	router.Handle("/admin/repository/{rid:[0-9]+}/notification/create", chainAdmin(viewAdminCreateNotification))
+	router.Handle("/admin/repository/{rid:[0-9]+}/notification/{nid:[0-9]+}", chainAdmin(viewAdminUpdateNotification))
+	router.Handle("/admin/repository/{rid:[0-9]+}/notification/delete/{nid:[0-9]+}", chainAdmin(viewAdminDeleteNotification))
 
-	router.Handle("/admin/repository/{rid:[0-9]+}/command/create", chainAdmin.ThenFunc(viewAdminCreateCommand))
-	router.Handle("/admin/repository/{rid:[0-9]+}/command/{cid:[0-9]+}", chainAdmin.ThenFunc(viewAdminUpdateCommand))
-	router.Handle("/admin/repository/{rid:[0-9]+}/command/delete/{cid:[0-9]+}", chainAdmin.ThenFunc(viewAdminDeleteCommand))
+	router.Handle("/admin/repository/{rid:[0-9]+}/command/create", chainAdmin(viewAdminCreateCommand))
+	router.Handle("/admin/repository/{rid:[0-9]+}/command/{cid:[0-9]+}", chainAdmin(viewAdminUpdateCommand))
+	router.Handle("/admin/repository/{rid:[0-9]+}/command/delete/{cid:[0-9]+}", chainAdmin(viewAdminDeleteCommand))
 
 	// add rice box to serve static files. Do not use the full middleware stack but
 	// only the logging handler. We do not want "notConfigured" to run e.x. so we
